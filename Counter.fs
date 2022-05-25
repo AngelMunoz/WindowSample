3765a865b6e5bcdd1ea50d5ec8dd4d3ccc94107d
Angel D. Munoz <scythes.lol@gmail.com>
2022-05-25 15:27:02 -0500

    first commit

---
+++ Counter.fs
@@ -0,0 +1,64 @@
+﻿namespace WindowSample
+
+module Counter =
+    open Avalonia.Controls
+    open Avalonia.FuncUI
+    open Avalonia.FuncUI.DSL
+    open Avalonia.Layout
+    open System.Collections.Generic
+
+    let view (window: Window) =
+
+        Component(fun ctx ->
+            let filePaths = ctx.useState []
+
+            let handleClickAsync () =
+                let dialog = OpenFileDialog()
+                dialog.AllowMultiple <- true
+
+                let filter =
+                    FileDialogFilter(Extensions = List<string>([ "md" ]), Name = "Markdown Files")
+
+                dialog.Filters.Add(filter)
+                dialog.Title <- "Select Markdown Files"
+
+                async {
+                    let! files = dialog.ShowAsync window |> Async.AwaitTask
+                    files |> Seq.toList |> filePaths.Set
+                }
+
+            let handleClickTask () =
+                let dialog = OpenFileDialog()
+                dialog.AllowMultiple <- true
+
+                let filter =
+                    FileDialogFilter(Extensions = List<string>([ "md" ]), Name = "Markdown Files")
+
+                dialog.Filters.Add(filter)
+                dialog.Title <- "Select Markdown Files"
+
+                task {
+                    let! files = dialog.ShowAsync window
+                    files |> Seq.toList |> filePaths.Set
+                }
+
+            StackPanel.create
+                [
+                    StackPanel.verticalAlignment VerticalAlignment.Center
+                    StackPanel.horizontalAlignment HorizontalAlignment.Center
+                    StackPanel.children
+                        [
+                            Button.create
+                                [
+                                    Button.content "Open Dialog Using Async"
+                                    Button.onClick (fun _ -> handleClickAsync () |> Async.Start)
+                                ]
+                            Button.create
+                                [
+                                    Button.content "Open Dialog Using Task"
+                                    Button.onClick (fun _ -> handleClickTask () |> ignore)
+                                ]
+                            for file in filePaths.Current do
+                                TextBlock.create [ TextBlock.text file ]
+                        ]
+                ])
